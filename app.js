11b6b417561dbee226840aec153d1edc9dda1ec6
Revoj-a <revojjoever@gmail.com>
2025-07-22 19:16:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document
+  .querySelector("img")
+  .setAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200"
+  );
+
+document.querySelector("img").alt = "Chicken";
